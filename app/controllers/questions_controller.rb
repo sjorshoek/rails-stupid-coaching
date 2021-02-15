a97b3dd016f5d8cda32d899fb50f66b4f580e536
sjors hoek <sjors.hoek@hotmail.com>
2021-02-15 16:07:57 +0100

    stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params['question'] == 'I am going to work'
+      @coach_answer = 'Great!'
+    elsif params['question'].include?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!.'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
